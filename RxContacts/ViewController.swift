c9f7492a997f143146a0146f214c3b27c4449104
Bryan <bolivarbryan@gmail.com>
2017-08-07 17:28:39 -0500

    Initial Commit

---
+++ RxContacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxContacts
+//
+//  Created by Bryan A Bolivar M on 8/7/17.
+//  Copyright © 2017 bolivarbryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
